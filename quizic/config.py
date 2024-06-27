886f34c32122185f88012632335f15b5de42eb90
affan <affanabid31@gmail.com>
2024-06-27 19:13:36 +0500

    code

--- quizic/config.py
+++
@@ -1,4 +0,0 @@
-CLIENT_ID = '1a38052d065540548b253c0920ee0a20'
-CLIENT_SECRET = '138f810abcd14faa81849e33741384ac'
-REDIRECT_URI = 'http://localhost:5000/callback'
-SCOPE = 'user-read-playback-state user-modify-playback-state'
