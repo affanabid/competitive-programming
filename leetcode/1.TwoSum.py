94ed9a82d3e4131dc50825335f60f5f11cb8c6ff
affan <affanabid31@gmail.com>
2024-07-14 15:09:24 +0500

    Day-15

---
+++ leetcode/1.TwoSum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums2 = sorted(nums)
+        i = 0
+        n = len(nums2) -1
+        while i < n:
+            sm = nums2[i] + nums2[n]
+            if sm == target:
+                i = nums.index(nums2[i])
+                nums[i] = -1
+                n = nums.index(nums2[n])
+                return i, n
+
+            elif sm > target:
+                n -= 1
+            elif sm < target:
+                i += 1
+            
