1b1bdfb0cd0da1add7521ff20b5bf72e99173c89
affan <affanabid31@gmail.com>
2024-07-20 13:28:13 +0500

    Day-21

---
+++ leetcode/876.MiddleOfLinkedList.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head):
+        curr1 = head
+        curr2 = head
+        while curr2 and curr2.next:
+            curr1 = curr1.next
+            curr2 = curr2.next.next
+        return curr1
+            
