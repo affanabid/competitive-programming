5dffff6d32498da3ddaef5db7bc4ce243e3f7ac2
affan <affanabid31@gmail.com>
2024-07-27 19:38:51 +0500

    Day-27

---
+++ leetcode/0-99/49.GroupAnagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        seen = {}
+        for s in strs:
+            so = "".join(sorted(s))
+            if so not in seen:
+                seen[so] = [s]
+            else:
+                seen[so].append(s)
+        return seen.values()
