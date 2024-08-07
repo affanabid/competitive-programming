0bac8580eb57415ba5511a7d598fc6093f442613
affan <affanabid31@gmail.com>
2024-08-07 23:12:11 +0500

    Day-38

---
+++ leetcode/0-99/78.Subsets.py
@@ -0,0 +1,22 @@
+class Solution:
+    def subsets(self, nums):
+        result = []
+
+        subset = []
+
+        def depthFirstSearch(i):
+            if i >= len(nums):
+                result.append(subset.copy())
+                return
+            
+            # include nums[i]
+            subset.append(nums[i])
+            depthFirstSearch(i + 1)()
+
+            # do not include nums[i]
+            subset.pop()
+            depthFirstSearch(i + 1)
+
+        depthFirstSearch(0)
+
+        return result
