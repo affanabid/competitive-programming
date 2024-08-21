edad5e9bb0e6114298d66ed1caf36ba346288c51
affan <affanabid31@gmail.com>
2024-08-21 10:15:45 +0500

    Day-46

---
+++ leetcode/100-199/101.SymmetricTree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root):
+        def helperSym(node1, node2):
+            if not node1 and not node2:
+                return True
+            if not node1 or not node2:
+                return False
+            return node1.val == node2.val and helperSym(node1.left, node2.right)and helperSym(node1.right, node2.left)
+
+        return helperSym(root.left, root.right)
