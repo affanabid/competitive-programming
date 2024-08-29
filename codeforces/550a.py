e5eb0a9118d837a7905861662eff8099a0c41f93
affan <affanabid31@gmail.com>
2024-08-29 22:45:33 +0500

    Day-50

--- codeforces/550a.py
+++ codeforces/550a.py
@@ -1,17 +1,21 @@
 def sol(s):
     count = 2
     i = 0
+    status1 = True
+    status2 = True
     while i < len(s) - 1:
-        if s[i] == 'A':
+        if s[i] == 'A' and status1:
             if s[i+1] == 'B':
+                status1 = False
                 count -= 1
                 i += 1
-        elif s[i] == 'B':
+        elif s[i] == 'B' and status2:
             if s[i+1] == 'A':
+                status2 = False
                 count -= 1
                 i += 1
         i += 1
-    if count == 0:
+    if count <= 0:
         return 'YES'
     else:
         return 'NO'
--- leetcode/0-99/88.MergeSortedArray.py
+++ leetcode/0-99/88.MergeSortedArray.py
@@ -1,3 +1,35 @@
-a = [0,1,2,3]
-b = a[::1]
-print(b)
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        numsC = nums1[::1]
+        pointer = 0
+        p1 = 0
+        p2 = 0
+        while p1 < m and p2 < n and pointer < (m+n):
+            if numsC[p1] == nums2[p2]:
+                nums1[pointer] = numsC[p1]
+                nums1[pointer+1] = nums2[p2]
+                pointer += 2
+                p1 += 1
+                p2 += 1
+            elif numsC[p1] < nums2[p2]:
+                nums1[pointer] = numsC[p1]
+                pointer += 1
+                p1 += 1
+            elif numsC[p1] > nums2[p2]:
+                nums1[pointer] = nums2[p2]
+                pointer += 1
+                p2 += 1
+        if p1 < m:
+            while p1 < m:
+                nums1[pointer] = numsC[p1]
+                pointer += 1
+                p1 += 1
+        if p2 < n:
+            while p2 < n:
+                nums1[pointer] = nums2[p2]
+                pointer += 1
+                p2 += 1
+        return nums1
